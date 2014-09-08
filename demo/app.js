8cfaeca51a2bc1e7e6cf1d27f9f469f6a698720d
Cameron Gallarno <cgallarno@gmail.com>
2014-09-08 12:56:42 -0400

    updated froala editor with type checking for returned html

--- demo/app.js
+++ demo/app.js
@@ -1,18 +1,17 @@
-	// create the module and name it myApp
+	// create the module with global defaults for froala
 	var myApp = angular.module('myApp', ['ngSanitize', 'da.froala']).
 		value('froalaConfig', {
 			inlineMode: false
 		});
 
 	// create the controller and inject Angular's $scope
-	myApp.controller('mainController', function($scope, $timeout) {
+	myApp.controller('mainController', function($scope) {
 		$scope.options = {
-			borderColor: '#0000ff',
-			disableRightClick : true,
-			placeholder : 'My Placeholder'
+			placeholder : 'My Placeholder',
 		};
 
 		$scope.froalaAction = function(action){
 			$scope.options.froala(action);
 		};
+
 	});
